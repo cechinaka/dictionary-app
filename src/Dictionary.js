788747ba9fc0eda339726a43aed3879be8d8bfb5
cechinaka <chinaka08@gmail.com>
2025-06-16 16:33:28 -0400

    added search box and keyword change state

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function search(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+
     return (
-    <div className="Dictionary">Hello</div>
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        </form>
+    </div>
     );
 }
